baa6826e29aa0fdcc9b5ab4c7fe88bfa535a564b
Bruce McLean <mcleanb@photo-nomad.net>
2018-11-26 07:29:18 -0800

    working on how to parse this for use in tCore.

--- src/strongsHelpers.js
+++ src/strongsHelpers.js
@@ -17,18 +17,67 @@ const outputPath = path.join(__dirname, '../resources/en/lexicons/uhl');
  */
 export async function generateStrongsFiles(version) {
   const versionPath = path.join(outputPath, version);
-  const inputPath = path.join(__dirname, '../DictBDB.json');
-  const dictBDB = fs.readJsonSync(inputPath);
-  for (let i = 0, len = dictBDB.length; i < len; i++) {
-    const item = dictBDB[i];
-    const {top: strongs, def} = item;
-    const data = convert('<html>' + def + '</html>', {
-        hebrew: text('ref0.word > font'),
-        strongs: text('div > p > b')
+  const index = [];
+
+  const inputPath = path.join(__dirname, '../unabridged-BDB-Hebrew-lexicon.csv');
+  const dictBDB = fs.readFileSync(inputPath).toString();
+  const dictLines = dictBDB.split('\n');
+  for (let i = 1, len = dictLines.length; i < len; i++) {
+    let dictLine = dictLines[i];
+    if (!dictLine) {
+      console.warn("Skipping Line: " + dictLine);
+      continue;
+    }
+    const cols = dictLine.split('\t');
+    const BDBid = cols[0];
+    const strongs = cols[1].split('_');
+    const content = cols[2];
+
+    const entry = {
+      brief: strongs,
+      long: content
+    };
+
+    for (let j= 0, sLen = strongs.length; j < sLen; j++) {
+      const strongsNum = strongs[j].substr(1);
+      if (strongsNum) {
+        const filePath = path.join(versionPath, 'content', strongsNum + ".json");
+        fs.outputJsonSync(filePath, entry);
+
+        const data = convert('<html>' + content + '</html>', {
+          hebrew: text('bdbheb')
+        });
+
+        const hebrew = (data.hebrew && data.hebrew.length) ? data.hebrew[0] : strongsNum; //TODO: verify correct word being parsed
+        
+        const indexEntry = {
+          id: strongsNum,
+          name: hebrew
+        };
+        index.push(indexEntry);
+        
+        console.log(strongsNum + ", word = " + indexEntry.name + ", definition= " + entry.long);
+      } else {
+        console.warn("Strongs number not found: " + dictLine);
       }
-    );
-    console.log("data= " + data);
+    }
   }
-  console.log(`Parsing Strongs`);
+
+  const filePath = path.join(versionPath, "index.json");
+  fs.outputJsonSync(filePath, index);
+  
+    // const inputPath = path.join(__dirname, '../DictBDB.json');
+  // const dictBDB = fs.readJsonSync(inputPath);
+  // for (let i = 0, len = dictBDB.length; i < len; i++) {
+  //   const item = dictBDB[i];
+  //   const {top: strongs, def} = item;
+  //   const data = convert('<html>' + def + '</html>', {
+  //       hebrew: text('ref0.word > font'),
+  //       strongs: text('div > p > b')
+  //     }
+  //   );
+  //   console.log("data= " + data);
+  // }
+  console.log(`finished Parsing`);
 }
 
